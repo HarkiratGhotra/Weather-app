cca49433b511a52f61d16aff295d3b2cd985471c
hadoop <hadoop@roomas-MacBook-Pro.local>
2018-04-07 01:38:33 -0400

    added setting

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -13,21 +14,32 @@ export class HomePage {
 		city:string;
 		state:string;
 	}
-  constructor(public navCtrl: NavController, private weatherProvider:WeatherProvider) {
+  constructor(public navCtrl: NavController, 
+    private weatherProvider:WeatherProvider,
+    private storage:Storage) {
 
   }
 
   // when ever the component loaded, this will load
   ionViewWillEnter(){
-  	this.location = {
-  		city: 'Miami',
-  		state: 'FL'
-  	}
-  	// remember this is going to send data stream, which is observable so we need to use subscribe
-  	this.weatherProvider.getweather(this.location.city,this.location.state).subscribe(weather => {
-  		console.log(weather);
-  		this.weather = weather.current_observation;
-  	})
+
+    // we do not want to use staic value, we want something from api,so using storage
+    this.storage.get('location').then((val) => {
+      if(val !=null){
+         //putting the value from jason into location 
+         this.location = JSON.parse(val);
+      }else {
+        this.location = {
+      city: 'Miami',
+      state: 'FL'
+    }
+      }
+      // remember this is going to send data stream, which is observable so we need to use subscribe
+    this.weatherProvider.getweather(this.location.city,this.location.state).subscribe(weather => {
+      console.log(weather);
+      this.weather = weather.current_observation;
+    })
+    });
   }
 
 }
